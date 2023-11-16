1743c66acebe7f60c18c2100632630d91093f860
saberSakin <md.shakin18@gmail.com>
2023-11-17 00:48:38 +0600

    navabr update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
+import {
+  Navbar,
+  Loader,
+  Footer,
+  Services,
+  Welcome,
+  Transactions,
+} from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 };
